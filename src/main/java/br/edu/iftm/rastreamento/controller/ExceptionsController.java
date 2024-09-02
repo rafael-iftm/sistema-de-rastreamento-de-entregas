b858aa08e5a1b35dea10195a3102a6e2d8df60f8
Rafael <142061101+rafael-iftm@users.noreply.github.com>
2024-09-02 20:50:42 -0300

    Update ExceptionsController.java

--- src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
+++ src/main/java/br/edu/iftm/rastreamento/controller/ExceptionsController.java
@@ -5,14 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.edu.iftm.rastreamento.service.exceptions.NaoAcheiException;
+import br.edu.iftm.rastreamento.service.exceptions.pacoteException;
 
 @ControllerAdvice
 public class ExceptionsController {
 
-	@ExceptionHandler(NaoAcheiException.class)
-	public ResponseEntity<?> naoAchei(NaoAcheiException e) {
-		System.out.println("------------------------");
+	@ExceptionHandler(pacoteException.class)
+	public ResponseEntity<?> naoAchei(pacoteException e) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 	}
 }
